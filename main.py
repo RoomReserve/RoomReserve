c4fe95d2e308f4bcb6594f8f5cf2204445d16977
Dorjee Dhondup <dhondo01@luther.edu>
2015-09-22 00:36:50 -0500

    Test template rendering
    Template working, bootstrap works too.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import *
+from jinja2 import Template
 app = Flask(__name__)
-
-app.run(debug=True) 
-'''enable debugging '''
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    # return "Hello World!"
+    title = "Hello Template"
+    p = "Hello World! This is a test page!"
+    return render_template('test.html', title=title,p=p)
 
 if __name__ == "__main__":
     app.run()
