a9f73a605ec15e305cf2ad4265a9c24a6f17f5f1
Ryan Bennett <bennry01@luther.edu>
2015-09-24 15:28:39 -0500

    Homepage, error pages
    jinja templates are working.

--- main.py
+++ main.py
@@ -11,11 +11,43 @@ db = SQLAlchemy(app)
 
 
 @app.route("/")
-def hello():
+def page_homepage():
+	title="RoomReserve Homepage"
+	content = "<strong>Welcome to RoomReserve</strong>"
+	return render_template('basic.html', title=title, content=content)
+
+
+@app.route("/today")
+def page_today():
+	title="Today's Activity"
+	return render_template('basic.html',title=title)
+
+
+
+
+@app.route("/hello")
+def page_hello():
     # return "Hello World!"
     title = "Test Works"
     p = "Hello World!"
     return render_template('test.html', title=title,p=p)
+
+
+
+
+#----Error Pages----
+@app.errorhandler(404)
+def page_error404(e):
+	desc="It looks like the page you were looking for doesn't exist or has been moved.<br />Sorry about that!"
+	head="404 - Page Not Found"
+	return render_template('error.html',desc=desc,head=head), 404
+
+@app.errorhandler(500)
+def page_error500(e):
+	desc="Something major has gone wrong. You should check your log files. 500 INTERNAL SERVER ERROR"
+	head="500 - Database error"
+	return render_template('error.html',desc=desc,head=head), 500
+
 
 if __name__ == "__main__":
     app.run()
