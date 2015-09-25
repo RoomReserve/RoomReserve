a7d9caa1eb5cf9625792b97a3ae9e4d1b15e1248
Dorjee Dhondup <dhondo01@luther.edu>
2015-09-24 20:10:44 -0500

    Remove duplicate codes
    The error handler pages are duplicates. There were two 404 and two 500
    error handlers. Dups were removed.

--- main.py
+++ main.py
@@ -78,19 +78,6 @@ def page_error500(e):
 
 
 
-#----Error Pages----
-@app.errorhandler(404)
-def page_error404(e):
-	desc="It looks like the page you were looking for doesn't exist or has been moved.<br />Sorry about that!"
-	head="404 - Page Not Found"
-	return render_template('error.html',desc=desc,head=head), 404
-
-@app.errorhandler(500)
-def page_error500(e):
-	desc="Something major has gone wrong. You should check your log files. 500 INTERNAL SERVER ERROR"
-	head="500 - Database error"
-	return render_template('error.html',desc=desc,head=head), 500
-
 
 if __name__ == "__main__":
     app.run()
