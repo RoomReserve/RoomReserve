9163a461e284cbe5fa617c821d6fd0853e803f27
Dorjee Dhondup <dhondo01@luther.edu>
2015-09-27 00:41:15 -0500

    change sqlalchemy database uri
    Add ‘sqlite:///:memory:'

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 db = SQLAlchemy(app)
 
 # User
