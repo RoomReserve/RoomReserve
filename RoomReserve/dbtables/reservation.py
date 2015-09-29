32aac3728f6378cbea4b70c283ad660fc3a8440b
Ryan Bennett <bennry01@luther.edu>
2015-09-29 16:13:38 -0500

    Gave the SQLAlchemy classes their own classes

---
+++ RoomReserve/dbtables/reservation.py
@@ -0,0 +1,21 @@
+from RoomReserve import *
+
+class Reservation(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	guest = db.Column(db.Integer, ForeignKey("Guest.id"), nullable=False)
+	madeby = db.Column(db.Integer, ForeignKey("User.id"), nullable=False)
+	place = db.Column(db.Integer, ForeignKey("Room.id"), nullable=False)
+	time = db.Column(datetime.datetime, nullable=False, unique=False)
+	status = db.Column(db.String(20), unique=False, nullable=False)
+	notes = db.Column(db.String(500), unique=False)
+
+	def __init__(self, guest, madeby, place, time, status="Unarrived", notes=""):
+		self.guest = guest
+		self.madeby = madeby
+		self.place = place
+		self.time = time
+		self.status = status
+		self.notes = notes
+
+	def __repr__(self):
+		return '%r %r' % (self.time, self.place)
