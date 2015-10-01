26e9def13eab766e7ab42181226694ddac23f556
stekza01 <stekza01@luther.edu>
2015-10-01 15:42:51 -0500

    Update __init__.py

--- RoomReserve/__init__.py
+++ RoomReserve/__init__.py
@@ -1,9 +1,10 @@
 from flask import *
 from jinja2 import Template
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask_wtf import Form
-from wtforms import *
-from wtforms.validators import *
+
+#these are imported for the engine and session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 #Start flask instance
 app = Flask(__name__)
@@ -20,6 +21,20 @@ from RoomReserve.dbtables.user import User
 from RoomReserve.dbtables.guest import Guest
 from RoomReserve.dbtables.room import Room
 from RoomReserve.dbtables.reservation import Reservation
+
+
+#this will be helpful for engine reference: http://docs.sqlalchemy.org/en/rel_0_9/core/engines.html
+
+# an Engine, which the Session will use for connection
+# resources
+some_engine = create_engine('postgresql://scott:tiger@localhost/')
+#we will need to change the target of the create_engine
+
+# create a configured "Session" class, this is our "session factory" it is recommended that we keep this at the global scope.
+Session = sessionmaker(bind=some_engine)
+
+# the following is an example on how to create a Session, I don't believe we need this at a global scope but can have multiple.
+#session = Session()
 
 
 # Creates database classes as defined in the
@@ -48,20 +63,6 @@ def db_test():
     title = "DB Works!"
     return render_template('test.html', title=title, users=users)
 
-class test_form(Form):
-    name = StringField('name', validators=[DataRequired()])
-
-@app.route("/wtftest", methods=['GET', 'POST'])
-def wtf_test():
-    if request.method == 'GET':
-        form = test_form()
-        return render_template('formtest.html', form=form)
-    elif request.method == 'POST':
-        content = "hello "
-        content += request.form['name']
-        return render_template('basic.html', content=content)
-    else:
-        return RoomReserve.helpers.errorhandlers.page_error400(400)
 
 @app.route("/today")
 def page_today():
