28c6061a348fadbc91d135c92bdf5be96ea7f337
Ryan Bennett <bennry01@luther.edu>
2015-10-08 16:17:29 -0500

    /admin/users page lists users

--- RoomReserve/admin/user.py
+++ RoomReserve/admin/user.py
@@ -14,24 +14,42 @@ class form_CreateUser(Form):
 
 @app.route('/admin/users', methods=['GET', 'POST'])
 def page_users():
-    if request.method == 'GET':
-        form = form_CreateUser()
-        return render_template('users.html', form=form)
-    elif request.method == 'POST':
+
+    if request.method == 'POST':
         formdata = request.form
         firstname = formdata['firstname']
         lastname = formdata['lastname']
         email = formdata['email']
         password = formdata['password']
         role = formdata['role']
+
         if createUser(firstname, lastname, email, role):
-            content = "hello "
-            content += firstname
-            return render_template('basic.html', content=content)
+            #user created sucessfully
+            pass
         else:
+            #could not create user
             return render_template('basic.html', content="Could not create user.")
-    else:
-        return RoomReserve.helpers.errorhandlers.page_error400(400)
+
+    form = form_CreateUser()
+    users = getAllUsers()
+    return render_template('users.html', form=form, users=users)
+
+def getAllUsers():
+    #returns all users in a dictionary
+    users = []
+    for me in db.session.query(User):
+    	users.append(me)
+    return users
+
+def getUser(myemail):
+    #returns single user object with the given email
+    users = []
+    for me in db.session.query(User).filter_by(email=myemail):
+    	users.append(me)
+    if len(users) == 1:
+        return users[0]
+    return False
+
 
 def createUser(fn, ln, em, ro):
     try:
