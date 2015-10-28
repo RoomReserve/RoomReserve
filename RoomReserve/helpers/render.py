ffdefd2424d0b58811236e7ce9a0dfb9b6893201
Ryan Bennett <bennry01@luther.edu>
2015-10-27 23:41:28 -0500

    Added feel-good messages

--- RoomReserve/helpers/render.py
+++ RoomReserve/helpers/render.py
@@ -1,7 +1,13 @@
 from RoomReserve import *
 
+import random
+
+feelings = ["magic", "love", "happiness", "friendship", "smiles", "laughter"]
+
 def render(template, *args, **kwargs):
     kwargs['withGlobals']=True
     kwargs['appname']='RoomReserve'
     kwargs['build'] = 'in development. Running on ' + sys.platform
+    kwargs['feeling'] = feelings[random.randrange(0,len(feelings))]
+
     return render_template(template, *args, **kwargs)
