2b8f4dbb0e78ace4f6d49f8b2e2999165791ebfb
Ryan Bennett <bennry01@luther.edu>
2015-10-27 23:03:56 -0500

    Added warning for old render_template pages

--- RoomReserve/helpers/render.py
+++ RoomReserve/helpers/render.py
@@ -1,6 +1,7 @@
 from RoomReserve import *
 
 def render(template, *args, **kwargs):
+    kwargs['withGlobals']=True
     kwargs['appname']='RoomReserve0.1'
     kwargs['build'] = 'in development. Running on ' + sys.platform
     return render_template(template, *args, **kwargs)
