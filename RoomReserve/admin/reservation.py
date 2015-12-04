0acb62fe0a01659da7b5700d2805bd8cb2faf432
stekza01 <stekza01@luther.edu>
2015-12-03 20:56:55 -0600

    I can make changes again!!!!

--- RoomReserve/admin/reservation.py
+++ RoomReserve/admin/reservation.py
@@ -8,9 +8,6 @@ class form_CreateReservation(Form):
     username = IntegerField('Your user id', validators=[DataRequired()])
     roomID = IntegerField('Room ID', validators=[DataRequired()])
 
-
-
-    #the lists below may need to be created into tuples............... Yeah. We'll see.
     minuteList = []
     hourList = []
     monthList = [(1, "January"), (2, "February"), (3, "March"), (4, "April"), (5, "May"), (6, "June"), (7, "July"), (8, "August"), (9, "September"), (10, "October"), (11, "November"), (12, "December")]
