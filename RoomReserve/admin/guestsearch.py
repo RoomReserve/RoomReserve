167561a93784df11b7359ab52556e2f8dc4bfcda
Dorjee Dhondup <dhondo01@luther.edu>
2015-12-06 00:16:16 -0600

    Add conditions for guest search by phone

--- RoomReserve/admin/guestsearch.py
+++ RoomReserve/admin/guestsearch.py
@@ -6,6 +6,7 @@ class form_SearchGuest(Form):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
     email = StringField('Email Address', validators=[DataRequired()])
+    phone = StringField('Phone Number', validators=[DataRequired()])
     
 
 # Get all the guest info from the database
@@ -30,10 +31,15 @@ def guestsearch():
     form = form_SearchGuest()
 
     if request.method == 'POST':
-        if 'firstname' or 'lastname' or 'email' in request.form: 
+        if 'firstname' or 'lastname' or 'email' or 'phone' in request.form: 
             firstname = request.form['firstname']
             lastname = request.form['lastname']
             email = request.form['email']
+            phone = ""
+            for char in request.form['phone']:
+                if char in "0123456789":
+                    phone += char
+            # phone = int(phone)
 
             # Add support for search by lowercase name
             firstname = firstname.capitalize()
@@ -41,18 +47,29 @@ def guestsearch():
 
 
             guests = []
+            if firstname and lastname and email and phone:
+                guests = guests + getGuestByName(first=firstname,last=lastname,email=email, phone=phone)
 
-            if firstname and lastname and email:
-                guests = guests + getGuestByName(first=firstname,last=lastname,email=email)
+            elif firstname and lastname and email:
+                guests = guests + getGuestByNameAndEmail(first=firstname,last=lastname,email=email)
 
-            if firstname and lastname:
+            elif firstname and lastname and phone:
+                guests = guests + getGuestByNameAndPhone(first=firstname,last=lastname,phone=phone)
+
+            elif firstname and lastname:
                 guests = guests + getGuestByName(first=firstname,last=lastname)
 
-            if firstname and email:
+            elif firstname and email:
                 guests = guests + getGuestByFirstNameAndEmail(first=firstname,email=email)
 
-            if lastname and email:
-                guests = guests + getGuestByLastName(last=lastname,email=email)
+            elif firstname and phone:
+                guests = guests + getGuestByFirstNameAndPhone(first=firstname,phone=phone)
+
+            elif lastname and email:
+                guests = guests + getGuestByLastNameAndEmail(last=lastname,email=email)
+
+            elif lastname and phone:
+                guests = guests + getGuestByFirstNameAndPhone(last=lastname,phone=phone)
 
             elif firstname:
                 guests = guests + getGuestByFirstName(firstname)
@@ -62,6 +79,9 @@ def guestsearch():
 
             elif email:
                 guests = guests + getGuestByEmail(email)
+
+            elif phone:
+                guests = guests + getGuestByPhone(phone)
 
             return render('guestsearch.html', form=form, guests=guests)
 
