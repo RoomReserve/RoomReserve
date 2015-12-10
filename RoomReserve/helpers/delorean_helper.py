8c5fe9a569fa89af410fbec13914ed5ba03ce972
Ryan Bennett <bennry01@luther.edu>
2015-12-10 16:59:44 -0600

    Fixed a bug that would cause deloreans to include hour values

--- RoomReserve/helpers/delorean_helper.py
+++ RoomReserve/helpers/delorean_helper.py
@@ -4,7 +4,11 @@ from delorean import Delorean, range_daily
 
 def create_delorean(dStart, dEnd, includeLastDay=False):
     #TODO ADD SUPPORT FOR includeLastDay
-    return list(delorean.range_daily(start=dStart,stop=dEnd))
+    delor = list(delorean.range_daily(start=dStart,stop=dEnd))
+    datesOnly=[]
+    for dt in delor:
+        datesOnly.append(dt.date)
+    return datesOnly
 
 def delorean_crash(delor1, delor2):
     for day1 in delor1:
