e60c3f1aa321b4829a3a1010301bedaf182f3cee
Ryan Bennett <bennry01@luther.edu>
2015-12-21 14:58:07 -0600

    Added room selection to reswiz

--- RoomReserve/reservationwizard.py
+++ RoomReserve/reservationwizard.py
@@ -1,5 +1,7 @@
 from RoomReserve import *
-from RoomReserve.admin.reservation import find_available_rooms
+from RoomReserve.admin.reservation import find_available_rooms, createReservation, getReservationByID
+from RoomReserve.admin.rooms import getRoomByID
+from RoomReserve.admin.building import getBuildingById
 
 class Form_Wizard_Form(Form):
     '''
@@ -21,7 +23,15 @@ def page_reservation_wizard():
 
 @app.route('/res/step-2', methods=['POST'])
 #@Login.standard_required
-def page_reservation_wizard():
+def page_reservation_wizard_2():
+    '''
+    Processes the data from the first page
+    which includes the desired check in, out dates
+    and minimum capacity
+
+    Displays a list of rooms that are available
+    during the time and fit the criteria
+    '''
     formdata = request.form
     indate = formdata['check_in_date']
     outdate = formdata['check_out_date']
@@ -31,4 +41,33 @@ def page_reservation_wizard():
     outdate = delorean.parse(outdate).naive()
 
     availableRooms = find_available_rooms(indate, outdate)
-    return render('reswizard/wizard2.html', rooms=availableRooms)
+
+    myRes = createReservation(guestID=-1, madeby=current_user.getID(), roomID=-1, \
+                        checkin=indate, checkout=outdate, status=CONST.draft_status)
+    return render('reswizard/wizard2.html', rooms=availableRooms, resID=myRes.getID())
+
+@app.route('/res/step-3', methods=['POST'])
+def page_reservation_wizard_3():
+    '''
+    Updates the reservation with the selected room.
+
+    Asks for the guest information
+    '''
+    formdata = request.form
+    res = getReservationByID(int(formdata['resID']))
+    room = getRoomByID(int(formdata['roomID']))
+
+    res.setRoom(room=room)
+    buildingName = getBuildingById(room.get_building_id()).get_name()
+
+    '''
+    TODO: Make guest information form.
+    Allow option to find an existing guest
+    or create a new one
+    '''
+
+
+
+
+
+    return render('reswizard/wizard3.html', room=getRoomByID(res.getRoomID()), buildingName=buildingName)
