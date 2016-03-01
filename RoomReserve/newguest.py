84ba01739c094ad3d746a7e11d4f7a076afc01a5
Dorjee Dhondup <dhondo01@luther.edu>
2016-03-01 15:29:36 -0600

    Add support for newguest creation without login required

---
+++ RoomReserve/newguest.py
@@ -0,0 +1,30 @@
+from RoomReserve import *
+from RoomReserve.admin.guest import processCreateGuestForm, getAllGuests 
+
+class form_CreateGuest(Form):
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired()])
+    phone = StringField('Phone Number', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    payment = StringField('Payment', validators=[DataRequired()])
+    notes = TextAreaField('Notes')
+
+
+@app.route('/newguest', methods=['GET', 'POST'])
+def newguest():
+
+    form = form_CreateGuest()
+
+    if request.method == 'POST':
+        # the form has been filled out, import the data
+        if processCreateGuestForm(request.form):
+            # guest created sucessfully
+            pass
+        else:
+            # createGuest returned false, the guest could not be created.
+            return render('basic.html', content="Could not create guest.")
+
+
+    guests = getAllGuests()
+    return render('newguest.html', form=form, guests=guests)
