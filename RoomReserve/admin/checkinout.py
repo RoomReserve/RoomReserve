fc3d5e7a288445d7f7c9dcbf13171e8345c0f060
stekza01 <stekza01@luther.edu>
2016-04-05 14:53:47 -0500

    Update checkinout.py

--- RoomReserve/admin/checkinout.py
+++ RoomReserve/admin/checkinout.py
@@ -1,13 +1,14 @@
 from RoomReserve import *
 from RoomReserve.helpers.stats import listOfReservationsCheckingInToday, listOfReservationsCheckingOutToday
 from RoomReserve.admin.guest import getGuestByID
+from RoomReserve.admin.rooms import getRoomByID
 
 @app.route('/admin/checkin', methods=['GET','POST'])
 def checkin_page():
   reslist = listOfReservationsCheckingInToday()
-  return render('checkinout.html', reservations=reslist, checkInType=True, getGuestByID=getGuestByID)
+  return render('checkinout.html', reservations=reslist, checkInType=True, getGuestByID=getGuestByID, getRoomByID=getRoomByID)
   
 @app.route('/admin/checkout', methods=['GET','POST'])
 def checkout_page():
   reslist = listOfReservationsCheckingOutToday()
-  return render('checkinout.html', reservations=reslist, checkInType=False, getGuestByID=getGuestByID)
+  return render('checkinout.html', reservations=reslist, checkInType=False, getGuestByID=getGuestByID, getRoomByID=getRoomByID)
