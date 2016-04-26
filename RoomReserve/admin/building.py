43c3bee7d68e190c8822dc4bd39dd3cc1015799e
stekza01 <stekza01@luther.edu>
2016-04-26 14:59:32 -0500

    Update building.py

--- RoomReserve/admin/building.py
+++ RoomReserve/admin/building.py
@@ -1,7 +1,6 @@
 from RoomReserve import *
 import RoomReserve.helpers.login as Login
-from RoomReserve.dbtables.building import Building
-from RoomReserve.dbtables.room import Room
+
 
 class form_CreateBuilding(Form):
     name = StringField('Building Name', validators=[DataRequired()])
