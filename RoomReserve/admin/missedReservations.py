d238b6322a7d66192614546405d45ef1a4037ed2
stekza01 <stekza01@luther.edu>
2016-05-05 11:32:45 -0500

    Create missedReservations.py

---
+++ RoomReserve/admin/missedReservations.py
@@ -0,0 +1,12 @@
+from RoomReserve import *
+from RoomReserve.helpers.stats import listOfReservationsCheckingInToday, listOfReservationsCheckingOutToday
+from RoomReserve.admin.guest import getGuestByID
+from RoomReserve.admin.rooms import getRoomByID
+from RoomReserve.admin.reservation import getReservationByID, getMissedReservations
+import datetime
+
+@app.route('/admin/missed', methods=['GET','POST'])
+@Login.standard_required
+def missed_res_page():
+    reslist = getMissedReservations(datetime.date.today())
+    return render('missedReservations.html', reservations=reslist)
